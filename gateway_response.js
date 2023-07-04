7391226db572869771097e7122bb443583334345
Andrey Usmanov <57061541+Intralise@users.noreply.github.com>
2023-07-04 22:24:43 +0300

    gateway_response.js

--- gateway_response.js
+++ gateway_response.js
@@ -2,9 +2,8 @@
     $(document).ready(function ($) {
       $("#gateway_button").click(function (e) {
         e.preventDefault();
-  
-		  console.log('test')
-		  
+	
+	//Выполняем запрос к backend"у сайта	  
         $.ajax({
           url: "/wp-admin/admin-ajax.php",
           type: 'POST',
@@ -14,7 +13,8 @@
             Pragma: "no-cache",
             Expires: "0",
           },
-          data: {
+	 //конкретно здесь мы говорим что обращаемся к такой-то функции в backend"е
+          data: { 
             action: 'paymtech_process_payments',
           },
           success: function (results) {
@@ -25,71 +25,3 @@
       });
     });
   })(jQuery);
-
-  (function ($) {
-    $(document).ready(function ($) {
-      $("#order_id_check_button").click(function (e) {
-        e.preventDefault();
-        orderId=78428319430488138;
-		  console.log('test2');
-      console.log(orderId);
-		  
-      $.ajax({
-        url: "/wp-admin/admin-ajax.php",
-        type: 'POST',
-        cache: false,
-        headers: {
-          "Cache-Control": "no-cache, no-store, must-revalidate",
-          Pragma: "no-cache",
-          Expires: "0",
-        },
-        data: {
-          action: 'paymtech_check_order_status',
-          orderId: orderId,
-        },
-        success: function (results) {
-
-        },
-
-      });
-
-      });
-    });
-  })(jQuery);
-  
-  (function ($) {
-  document.addEventListener("DOMContentLoaded", function() {
-    var urlParams = new URLSearchParams(window.location.search);
-    var orderId = urlParams.get("order_id");
-    console.log("Заказ с идентификатором", orderId, "присутствует в URL");
-
-    
-
-    if (orderId) {
-      // Выполнить определенные действия, связанные с orderid
-      console.log("Заказ с идентификатором", orderId, "присутствует в URL");
-
-      $.ajax({
-        url: "/wp-admin/admin-ajax.php",
-        type: 'POST',
-        cache: false,
-        headers: {
-          "Cache-Control": "no-cache, no-store, must-revalidate",
-          Pragma: "no-cache",
-          Expires: "0",
-        },
-        data: {
-          action: 'paymtech_check_order_status',
-          orderId: orderId,
-        },
-        success: function (results) {
-
-        },
-
-      });
-    
-
-
-    }
-  });
-})(jQuery);
